b6a0f9c0ef972870d77baa3f9f0579d59e7ef925
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-06-07 22:45:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,15 @@
-# Add  code here!
-def prime(numbers)
-
-if numbers.prime
-  true
-else
-  false
+def prime?(x)
+  if x == 1
+    return true 
+  elsif x < 1
+    return false
+  else
+  for y in 2..(x-1)
+    if (x % y) == 0
+      return false 
+    end
+  end 
 end
 
-
+  true
 end
