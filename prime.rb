7555c5419857bc796c2e6c36ac77d08e0d0ec4ec
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-06-07 22:48:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,6 @@
 require prime
 
 def prime?(x)
-  if x == 1
-    return true
-  elsif x < 1
-    return false
-  else
-  for y in 2..(x-1)
-    if (x % y) == 0
-      return false
-    end
-  end
-end
-
-  true
+  
+prime.first(x)
 end
